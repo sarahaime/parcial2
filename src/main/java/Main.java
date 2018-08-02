c932f103cd5297bf9b91b02b0d3c806e64a9b2e2
sgiron <shantgiron@gmail.com>
2018-08-02 12:08:20 -0400

    Soap service OK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,24 @@
 import rutas.ManejoRutasGenerales;
 import rutas.ManejoRutasShant;
 import rutas.RutasImagen;
-import services.*;
+import services.BootStrapServices;
+import services.DB;
+import soap.SoapArranque;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.sql.SQLException;
 
 import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 public class Main {
-    public static void main(String[] args)throws SQLException {
+    public static void main(String[] args)throws Exception{
+        //Arranque del servidor
+        SoapArranque.stop();
+        SoapArranque.init();
+
+
         //Iniciando el servicio
         BootStrapServices.startDb();
 
@@ -27,29 +33,6 @@ public class Main {
         EntityManager entityManager = emf.createEntityManager();
         entityManager.getTransaction().begin();
         entityManager.close();
-
-
-        //usuario administrador por defecto
-//       UsuarioServices usuarioServices = new UsuarioServices();
-//        if(usuarioServices.listaUsuarios().size() < 1){
-//            Usuario administrador = new Usuario();
-//            administrador.setNombre("Nombre del Administrador");
-//            administrador.setUsername("admin");
-//            administrador.setAdministrador(true);
-//            administrador.setAutor(true);
-//            administrador.setPassword("admin");
-//            if( usuarioServices.crearUsuario(administrador)){
-//                System.out.println("Usuario administrador creado..");
-//            }
-//        }
-
-//        ArticuloServices as = new ArticuloServices();
-//        as.crearArticulo("El agua post", "cuerpesit00ao", 33, "comida, frio, sueño");
-//        EtiquetaServices es = new EtiquetaServices();
-//        es.crearEtiqueta("COCINA");
-//        es.crearEtiqueta("PROGRAMACION");
-//        es.crearEtiqueta("YOLO");
-//        es.crearEtiqueta("FOTOGRAFIA");
 
 
         //Seteando el puerto en Heroku
